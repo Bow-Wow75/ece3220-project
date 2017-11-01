12361ae5a7643c4983c7533e5482d2ad0ce8d137
Erik <erikbowers51@gmail.com>
2017-11-01 11:40:44 -0500

    rename main and add menu start

--- Stock-Program/src/main.cpp
+++ Stock-Program/src/main.cpp
@@ -1,15 +1,20 @@
 //============================================================================
 // Name        : Stock.cpp
-// Author      : Erik Bowers/Nicholas B
+// Author      : Erik Bowers & Nicholas B
 // Version     :
-// Copyright   : Your copyright notice
-// Description : Hello World in C++, Ansi-style
+// Copyright   :
+// Description : Stock trading program for ece 3220
 //============================================================================
 
 #include <iostream>
 using namespace std;
 
 int main() {
+	int action = 0;
+	while(action != -1)
+	{
+			//print menu
+	}
 	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
 	return 0;
 }
