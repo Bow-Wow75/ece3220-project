16ae5901157d9712fe71d2ce82ef826b4a65265b
Erik <erikbowers51@gmail.com>
2017-11-09 21:57:37 -0600

    all files combined into 1 in xcode directory since xcode and eclipse don't like each other

--- Stock-Program/src/FinalProject/Finalproject/Finalproject/main.cpp
+++ Stock-Program/src/FinalProject/Finalproject/Finalproject/main.cpp
@@ -26,18 +26,111 @@ class Account{
         string account_id;
         string account_password;
         int account_type;// 1 is admin, 5 is user
-    
+
     public:
         Account();
         ~Account();
         void login();
         void locate_account(string entered_id, string entered_password );
         void menu();
-    
-    
-    
-    
+
+
+
+
 };
+
+
+
+
+
+class Stock{
+private:
+
+protected:
+
+public:
+	double value;
+	std::string name;
+
+	void printStocks();
+	//constructor
+
+};
+
+
+class User{
+private:
+	double balance;
+	int id;
+	std::vector<Stock> stocks;
+	std::vector<int> stockNums;
+protected:
+
+public:
+
+};
+
+//.........END HEADER........//
+
+
+
+
+//..........from Erik's functions........//
+
+
+
+void Stock::printStocks()
+{
+	cout << name << "  " << value << endl;
+}
+
+
+
+vector<Stock> loadStocks()
+{
+
+
+		std::ifstream inputFile;
+		inputFile.open("stocks.txt");
+		//int fileError = 0;
+
+		if(!inputFile)
+		{
+
+			cerr << "Unable to open file" << endl;
+			throw 0;
+		}
+
+		vector<Stock> availableStocks;
+		Stock temp;
+
+		while(inputFile >> temp.name >> temp.value)
+		{
+			availableStocks.push_back(temp);
+		}
+
+		inputFile.close();
+		return availableStocks;
+
+}
+
+void printMenu()
+{
+	cout << "(1) See Available stocks: " << endl
+		 << "(2) Sell stocks: " << endl
+		 << "(3) Set Stock Seller Requirements" << endl
+		 << "(4) Account info: " << endl
+		 << "(5) Update account balance: " << endl;
+}
+
+
+
+//........End my functions.......//
+
+
+void printMenu();
+std::vector<Stock> loadStocks();
+
 Account::Account(){
     
 }
@@ -89,7 +182,7 @@ void Account:: locate_account(string entered_id, string entered_password ){
     int input_error = 0;
    string user_id, password;
     ifstream input;
-    input.open(input_file);
+    input.open(input_file.c_str());
     if( !input.is_open())
     {
         throw input_error;
@@ -140,4 +233,29 @@ int main( int argc, char** argv){
     
     
     
+	int action = 0;
+
+	try{
+	vector<Stock> availableStocks = loadStocks();
+
+	for(int i=0;i<availableStocks.size();i++)
+	{
+		availableStocks[i].printStocks();
+	}
+
+	}
+
+	catch(int fileError)
+	{
+		cout << "Could not open file" << endl;
+	}
+
+	while(action != -1)
+	{//print menu
+		printMenu();
+		cin >> action;
+	}
+
+
+
 }
